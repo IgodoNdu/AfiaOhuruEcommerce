9e06ce205964b4d66cd4c0cf4d85b46c68b9fe8b
IgodoNdu <darkbluetechmail@gmail.com>
2022-10-19 22:13:44 +0100

    Slider component (styled component styling)
    Building the slider component, styling done with styled components

---
+++ src/data.js
@@ -0,0 +1,24 @@
+//Array to hold slide items
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://burst.shopifycdn.com/photos/womens-tshirts.jpg?width=925&format=pjpg&exif=1&iptc=1",
+        title: "SPRING SALE",
+        desc: "DON'T MISS A GREAT OFFER! GET 20% OFF THIS WEEK.",
+        bg: "f5fafd",
+      },
+      {
+        id: 2,
+        img: "https://burst.shopifycdn.com/photos/womens-tshirts.jpg?width=925&format=pjpg&exif=1&iptc=1",
+        title: "AUTUMN SALE",
+        desc: "DON'T MISS A GREAT OFFER! GET 20% OFF THIS WEEK.",
+        bg: "fbf0f4",
+      },
+      {
+        id: 3,
+        img: "https://burst.shopifycdn.com/photos/womens-tshirts.jpg?width=925&format=pjpg&exif=1&iptc=1",
+        title: "REGULAR",
+        desc: "DON'T MISS A GREAT OFFER! GET 20% OFF THIS WEEK.",
+        bg: "fcf1ed",
+      },
+]
