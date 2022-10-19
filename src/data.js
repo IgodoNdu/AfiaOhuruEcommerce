32f03e713257d132100c001b7f5b6058bda0842b
IgodoNdu <darkbluetechmail@gmail.com>
2022-10-19 23:23:35 +0100

    Components for Categories and CategoryItem
    Creating and styling the Category item, and Categories components

--- src/data.js
+++ src/data.js
@@ -21,4 +21,29 @@ export const sliderItems = [
         desc: "DON'T MISS A GREAT OFFER! GET 20% OFF THIS WEEK.",
         bg: "fcf1ed",
       },
+];
+
+//Array to hold categories
+export const categories = [
+  {
+      id: 1,
+      img: "https://burst.shopifycdn.com/photos/womens-tshirts.jpg?width=925&format=pjpg&exif=1&iptc=1",
+      title: "SPRING SALE",
+    },
+    {
+      id: 2,
+      img: "https://burst.shopifycdn.com/photos/womens-tshirts.jpg?width=925&format=pjpg&exif=1&iptc=1",
+      title: "AUTUMN SALE",
+    },
+    {
+      id: 3,
+      img: "https://burst.shopifycdn.com/photos/womens-tshirts.jpg?width=925&format=pjpg&exif=1&iptc=1",
+      title: "REGULAR",
+    },
+    {
+      id: 4,
+      img: "https://burst.shopifycdn.com/photos/womens-tshirts.jpg?width=925&format=pjpg&exif=1&iptc=1",
+      title: "BEST PRICE",
+    },
 ]
+
